8bd66f83f67842db3f28b7b5b306b6d3d42cb446
Quertte <a.injen90@gmail.com>
2022-01-14 20:34:05 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -20,5 +20,24 @@ public class SqlRuParse {
                 System.out.println(parent.child(5).text());
             }
         }
+        System.out.println(description(
+                "https://www.sql.ru/forum/1341435/postgresql-developer-middle-300-net"));
+    }
+
+    private static String description(String url) throws IOException {
+        String description = null;
+        Document doc = Jsoup.connect(url).get();
+        Elements row = doc.select(".msgBody");
+        for (Element td : row) {
+            Element parent = td.parent();
+            description = parent.child(1).text();
+            break;
+        }
+        row = doc.select(".msgFooter");
+        for (Element td : row) {
+            description = description + "/" + td.text().substring(0, td.text().indexOf("["));
+            break;
+        }
+        return description;
     }
 }
