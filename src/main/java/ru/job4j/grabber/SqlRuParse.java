5fb98a8eae283779c5a71d3191e895ca4911df25
Quertte <a.injen90@gmail.com>
2022-01-15 18:22:14 +0300

    2.4. SqlRuParse [#285213 #251094] fix

--- src/main/java/ru/job4j/grabber/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/SqlRuParse.java
@@ -20,29 +20,42 @@ public class SqlRuParse implements Parse {
     }
 
     @Override
-    public List<Post> list(String link) throws IOException {
+    public List<Post> list(String link) {
         List<Post> posts = new ArrayList<>();
-        for (int i = 1; i < 6; i++) {
-            Document doc = Jsoup.connect(link + i).get();
-            Elements row = doc.select(".postslisttopic");
-            for (Element td : row) {
-                if (!td.child(0).text().toLowerCase().contains("javascript")
-                        && td.child(0).text().contains("Java")) {
-                    posts.add(detail(td.child(0).attr("href")));
+        try {
+            for (int i = 1; i < 6; i++) {
+                Document doc = Jsoup.connect(link + i).get();
+                Elements row = doc.select(".postslisttopic");
+                for (Element td : row) {
+                    if (!td.child(0).text().toLowerCase().contains("javascript")
+                            && td.child(0).text().toLowerCase().contains("java")) {
+                        posts.add(detail(td.child(0).attr("href")));
+                    }
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return posts;
     }
 
     @Override
-    public Post detail(String link) throws IOException {
-        Document doc = Jsoup.connect(link).get();
-        Elements row = doc.select(".msgBody");
-        String description = row.get(1).text();
-        row = doc.select(".msgFooter");
-        String date = row.first().text().substring(0, row.get(0).text().indexOf(" ["));
-        String name = doc.select(".messageHeader").get(0).ownText();
+    public Post detail(String link) {
+        Document doc;
+        Elements row;
+        String description = null;
+        String date = null;
+        String name = null;
+        try {
+            doc = Jsoup.connect(link).get();
+            row = doc.select(".msgBody");
+            description = row.get(1).text();
+            row = doc.select(".msgFooter");
+            date = row.first().text().substring(0, row.get(0).text().indexOf(" ["));
+            name = doc.select(".messageHeader").get(0).ownText();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return new Post(name, link, description, parser.parse(date));
     }
 }
